05c5b863f551fe6690719041bbfcbff5f9661e29
madhu-yagadasu <yagadasumadhu0306@gmail.com>
2024-09-01 00:40:19 +0530

    madhu's second commit

--- playwright.config.js
+++ playwright.config.js
@@ -18,8 +18,6 @@ module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
   /* Retry on First Failure  only */
   retries: 0,
   /* Opt out of parallel tests on CI. */
@@ -32,7 +30,6 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false,
     trace: 'retain-on-failure',
     screenshot:'only-on-failure',
     video:'retain-on-failure',
